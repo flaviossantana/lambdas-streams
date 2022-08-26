87041114e6570955e45ce148fa12814b5ce8e861
flavio.santana <9090flavio@gmail.com>
2022-08-25 21:21:48 -0300

    feat: código mais sucinto com method references

--- src/main/java/br/com/java/news/OrdeanarStrings.java
+++ src/main/java/br/com/java/news/OrdeanarStrings.java
@@ -1,24 +1,40 @@
 package br.com.java.news;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class OrdeanarStrings {
     public static void main(String[] args) {
 
         List<String> palavras = new ArrayList<>();
-        palavras.add("casa");
         palavras.add("carros");
+        palavras.add("flavio santana");
+        palavras.add("casa");
         palavras.add("motocas");
         palavras.add("barco");
-        palavras.add("flavio santana");
 
-        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
-        System.out.println(palavras);
-        palavras.forEach((palavra) -> System.out.println(palavra));
+        palavras.forEach(System.out::println);
+        System.out.println("----------------------------------------------------");
+
+        palavras.sort(String.CASE_INSENSITIVE_ORDER);
+        palavras.sort(Comparator.naturalOrder());
+        palavras.forEach(System.out::println);
+        System.out.println("----------------------------------------------------");
+
+        palavras.sort(Comparator.comparing(String::length));
+        palavras.forEach(System.out::println);
+        System.out.println("----------------------------------------------------");
+
+        List<String> slist = Arrays.asList("Tanu", "Kamal", "Suman", "Lucky", "Bunty", "Amit");
+        List<String> sortedList = slist.stream().sorted().collect(Collectors.toList());
+        sortedList.forEach(System.out::println);
+
+        //returns a list view
+        List<String> slistr = Arrays.asList("78", "a", "m", "b", "z", "c", "12", "l", "1");
+        List<String> sortedListr = slistr.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        sortedListr.forEach(System.out::println);
 
     }
 }
